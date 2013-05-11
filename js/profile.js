c560682b5381264ad938b947e4526d3bff99d977
Priyandiono <ifdion@gmail.com>
2013-05-11 07:01:02 +0700

    show or hide an element based on a checkbox condition

--- js/profile.js
+++ js/profile.js
@@ -1,6 +1,29 @@
 (function ($) {
 	"use strict";
 	$(function () {
-		// Place your public-facing JavaScript here
+		// show hide on init
+		if ($('[data-show-on]').length > 0) {
+			$('[data-show-on]').each(function(index){
+				var trigger = $(this).attr('data-show-on');
+				var trigger_object = $('[name="'+trigger+'"]');
+				trigger_object.addClass('trigger');
+				if (trigger_object.is(":checked")) {
+					$(this).show();
+				} else {
+					$(this).hide();
+				}
+			});
+		}
+
+		// show hide on change
+		$('.trigger').live('change',function(){
+			var target = $(this).attr('name');
+			var target_object = $('[data-show-on="'+target+'"]');
+			if ($(this).is(":checked")) {
+				target_object.show();
+			} else {
+				target_object.hide();
+			}
+		});
 	});
 }(jQuery));
